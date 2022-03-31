ccdcf6b5f73f9dbd411be8ed006118a55888ef10
itrytocodethings <imwaynebailey@gmail.com>
2022-03-31 00:36:21 +0000

    completed backend for todos

--- src/main.py
+++ src/main.py
@@ -21,18 +21,23 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/todos', methods=['GET', 'POST'])
 def handle_todos():
-    if request.method == "GET": 
+    if request.method == "GET":
         all_todos = Task.query.all()
         all_todos = list(map(lambda task: task.serialize(), all_todos))
         response_body = jsonify(all_todos)
@@ -46,6 +51,32 @@ def handle_todos():
         all_todos = list(map(lambda task: task.serialize(), all_todos))
         return jsonify(all_todos)
     return None
+
+
+@app.route('/todo/<int:id>', methods=['DELETE'])
+def delete_todo(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    all_todos = Task.query.all()
+    all_todos = list(map(lambda task: task.serialize(), all_todos))
+    return jsonify(all_todos)
+
+
+@app.route('/todo/<int:task_id>', methods=['PUT'])
+def update_todo(task_id):
+    task = Task.query.get(task_id)
+    req = request.json
+    if "label" in req:
+        task.label = req["label"]
+    if "done" in req:
+        task.done = req["done"]
+    db.session.commit()
+    all_todos = Task.query.all()
+    all_todos = list(map(lambda task: task.serialize(), all_todos))
+    return jsonify(all_todos)
+
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
@@ -54,6 +85,7 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
